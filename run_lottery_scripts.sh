fb28a815e3fe0a08bb49ed8c501488c406b76910
expiredyogurt <20634363+expiredyogurt@users.noreply.github.com>
2024-07-21 16:24:44 +0800

    Update run_lottery_scripts.sh

--- run_lottery_scripts.sh
+++ run_lottery_scripts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Activate Anaconda environment
-source /Users/kevinloke/anaconda3/bin/activate base
+source /Users/expiredyogurt/anaconda3/bin/activate base
 
 
 # Change directory to the location of the Python scripts for DMC
-cd /Users/kevinloke/Desktop/Lottery\ Project/DMC/
+cd /Users/expiredyogurt/Desktop/Lottery\ Project/DMC/
 
 # Run the Python script for DMC
 python3 "DMC TotoLotteryV2.py"
@@ -14,7 +14,7 @@ python3 "DMC Toto Prediction v4.py"
 python3 "DMC LSTM Top 3 Prediction.py"
 
 # Change directory to the location of the Python scripts
-cd /Users/kevinloke/Desktop/Lottery\ Project/
+cd /Users/expiredyogurt/Desktop/Lottery\ Project/
 
 # Run each Python script in sequence
 python3 "Download Past Toto Result.py"
@@ -24,7 +24,7 @@ python3 "SupremeTotoRandomForest.py"
 python3 "4D Toto Prediction v4.py"
 
 # Change directory to the location of the Python scripts for Magnum
-cd /Users/kevinloke/Desktop/Lottery\ Project/Magnum/
+cd /Users/expiredyogurt/Desktop/Lottery\ Project/Magnum/
 
 # Run the Python script for Magnum
 python3 "Magnum 4DTotoLotteryV2.py"
